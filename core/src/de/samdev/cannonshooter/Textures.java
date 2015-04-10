4d0366cc01f0594de87bb76127152e8bd4c3b3dd
Mike Schwörer <mailport@mikescher.de>
2015-04-10 14:10:32 +0200

    Added absGDX dependency

---
+++ core/src/de/samdev/cannonshooter/Textures.java
@@ -0,0 +1,6 @@
+package de.samdev.cannonshooter;
+
+public final class Textures {
+
+
+}
--- desktop/src/de/samdev/cannonshooter/desktop/DesktopLauncher.java
+++ desktop/src/de/samdev/cannonshooter/desktop/DesktopLauncher.java
@@ -2,11 +2,17 @@ package de.samdev.cannonshooter.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import de.samdev.absgdx.framework.util.AndroidResolutions;
 import de.samdev.cannonshooter.CannonGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.width  = AndroidResolutions.RES__16_9.x;
+		config.height = AndroidResolutions.RES__16_9.y;
+		
 		new LwjglApplication(new CannonGame(), config);
 	}
 }

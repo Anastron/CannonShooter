f5903884545e98a0129f683b11d573bdd67c9003
Mike Schwörer <mailport@mikescher.de>
2015-04-10 13:57:40 +0200

    Generated LibGDX project

---
+++ android/src/de/samdev/cannonshooter/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package de.samdev.cannonshooter.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.samdev.cannonshooter.CannonGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CannonGame(), config);
+	}
+}
---
+++ desktop/src/de/samdev/cannonshooter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.samdev.cannonshooter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.samdev.cannonshooter.CannonGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CannonGame(), config);
+	}
+}
